05e2a1feae5e391c9340db99f0258d94847f58ef
HazelnutStudio <hazelnutstudio.yt@gmail.com>
2024-07-29 19:07:29 +0800

    Time controls

--- Main.cs
+++ Main.cs
@@ -1,15 +1,14 @@
 ﻿using MelonLoader;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using UnityEngine;
 
 namespace DebugMod
 {
     public class Main : MelonMod
     {
+        const float STEP_TIME_AMOUNT = 0.1f;
+
+        private bool _steppingTime = false;
+        private float _stepTimer = 0;
         public override void OnInitializeMelon()
         {
             LoggerInstance.Msg("hey ;]");
@@ -24,8 +23,68 @@ namespace DebugMod
             {
                 SceneLog.LogSceneHierarchy();
             }
+
+            TimeControls();
         }
 
-        
+        private void TimeControls()
+        {
+            if (_steppingTime)
+            {
+                _stepTimer += Time.deltaTime;
+
+                if (_stepTimer >= STEP_TIME_AMOUNT)
+                {
+                    _steppingTime = false;
+                    Time.timeScale = 0;
+                }
+            }
+
+            if (Input.GetKeyDown(KeyCode.F2))
+            {
+                if (_steppingTime)
+                {
+                    return;
+                }
+
+                if (Time.timeScale == 0)
+                {
+                    Time.timeScale = 1;
+                }
+                else
+                {
+                    Time.timeScale = 0;
+                }
+
+                CLog.Info($"Timescale set to {Time.timeScale}");
+            }
+
+            if (Input.GetKeyDown(KeyCode.F3))
+            {
+                if (Time.timeScale == 0)
+                {
+                    Time.timeScale = 1;
+                    _steppingTime = true;
+                }
+                else if (_steppingTime == false)
+                {
+                    Time.timeScale -= 0.1f;
+                }
+
+                CLog.Info($"Timescale set to {Time.timeScale}");
+            }
+
+            if (Input.GetKeyDown(KeyCode.F4))
+            {
+                if (_steppingTime == true)
+                {
+                    return;
+                }
+
+                Time.timeScale += 0.1f;
+
+                CLog.Info($"Timescale set to {Time.timeScale}");
+            }
+        }
     }
 }
